b6d8c51578ec25276a433ead4f023b9e40b1fd8f
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-10-29 06:13:48 -0300

    enviando requisição com o método get

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,10 @@ const app = express()
 
 const PORT = 3333
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} \nhttp://localhost:${PORT}/ or \nhttp://192.168.0.55:${PORT}` )
 })
